534b578da4afaf467c678acf8c18b9e3a8a2998c
jessef <1319957+jfrausto@users.noreply.github.com>
2020-09-25 23:28:40 -0700

    update schema to allow deletes and updates

--- schema.sql
+++ schema.sql
@@ -1,8 +1,11 @@
 DROP DATABASE IF EXISTS employee_db;
-
 CREATE DATABASE employee_db;
 
 USE employee_db;
+
+DROP TABLE department;
+DROP TABLE role;
+DROP TABLE employee;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
@@ -15,29 +18,27 @@ CREATE TABLE role (
     PRIMARY KEY(id),
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
+    department_id INT,
     CONSTRAINT fk_department FOREIGN KEY (department_id)
         REFERENCES department(id)
-        ON DELETE RESTRICT ON UPDATE CASCADE
+        ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    role_id INT,
     manager_id INT,
     PRIMARY KEY(id),
 
     CONSTRAINT fk_role
     FOREIGN KEY (role_id)
         REFERENCES role(id)
-        ON DELETE RESTRICT ON UPDATE CASCADE,
+        ON DELETE SET NULL ON UPDATE CASCADE,
 
     CONSTRAINT fk_manager
     FOREIGN KEY (manager_id)
         REFERENCES employee(id)
-        ON DELETE RESTRICT ON UPDATE CASCADE
+        ON DELETE NO ACTION ON UPDATE NO ACTION
 );
-
-
