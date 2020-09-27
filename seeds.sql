658e7085eb66561eacdc719620f4e9a8d7f0716e
jessef <1319957+jfrausto@users.noreply.github.com>
2020-09-26 20:51:29 -0700

    minimum queries requirements met

--- seeds.sql
+++ seeds.sql
@@ -9,11 +9,10 @@ VALUES ("Manager", 64000, 4), ("Sales Agent", 42000, 1), ("HR Rep", 52000, 3),
 
 -- ! then add employees
 INSERT INTO employee(first_name, last_name, role_id, manager_id) 
-VALUES ("Sterling", "Archer", 1, NULL), ("Malory", "Archer", 1, NULL), 
-("Jesse", "Frausto", 4, 2), ("Pablo", "Escobar", 2, 2), 
-("Rosemary", "Frausto", 3, NULL);
+VALUES ("Sterling", "Archer", 1, NULL), ("Malory", "Archer", 1, NULL), ("Jesse", "Frausto", 4, 2);
 
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Eduardo", "Sanchez", 2, 2), ("Rosemary", "Frausto", 3, NULL);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id) 
 VALUES ("Lebron", "James", 5, 2), ("Javale", " McGee", 2, 6), 
-("Rajon", "Rondo", 3, 1), ("Anthony", "Davis", 5, 2), 
+("Rajon", "Rondo", 3, 1), ("Anthony", "Davis", 5, 2);
